1bcd422bc2c33da914abdddc4f9667a11967fe63
TheCaptain989 <thecaptain989@protonmail.com>
2022-08-30 14:23:52 -0500

    Release 2.3 candidate 1
    - Added Original language feature
    - Added :org special purpose language code (Resolves #392)
    - Added example scripts to use :org code
    - Cleaned up awk code
    - Modified removed track logging to be a single line
    - Removed tracks now are logged as Informational instead of Debug
    - Moved position argument check earlier
    - Updated README
    - Changed some log messages to make them more concise
    - Added version option
    - Reverted to single-stage build
    - Insert version into scripts during build
    - Updates Radarr/Sonarr version check to use v3 API. This makes the script fully compatible with Radarr v4.
    - Added debug info for system API
    - Added debug info for config API
    - Preparation for S6 V3

---
+++ root/usr/local/bin/striptracks-org-eng.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. /usr/local/bin/striptracks.sh --audio :org:eng:und --subs :eng:und
---
+++ root/usr/local/bin/striptracks-org-ger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. /usr/local/bin/striptracks.sh --audio :org:ger:deu --subs :org:ger:deu
