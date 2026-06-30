1ddc0fbe75818a3927e96179c78d9e3f34ffa096
quietsy <github@qtsy.cc>
2026-06-30 22:34:49 +0300

    Fix bug

--- root/app/swag-ondemand.py
+++ root/app/swag-ondemand.py
@@ -19,13 +19,13 @@ class ContainerThread(threading.Thread):
     def __init__(self):
         super().__init__()
         self.daemon = True
-        self.ondemand_containers = {}
         self.docker_hosts = []
         self.init_docker_hosts()
 
     def init_docker_hosts(self):
         try:
             docker_host = {}
+            docker_host["ondemand_containers"] = {}
             docker_host_url = os.environ.get("DOCKER_HOST", None)
             if docker_host_url:
                 if not docker_host_url.startswith("tcp://"):
@@ -41,6 +41,7 @@ class ContainerThread(threading.Thread):
             remote_hosts_env_vars = { key: value for key, value in os.environ.items() if key.startswith(REMOTE_HOSTS_PREFIX) }
             for i in range(1, 20):
                 remote_host = {}
+                remote_host["ondemand_containers"] = {}
                 if f"{REMOTE_HOSTS_PREFIX}{i}" not in remote_hosts_env_vars:
                     break
                 docker_host_url = remote_hosts_env_vars[f"{REMOTE_HOSTS_PREFIX}{i}"]
@@ -63,57 +64,62 @@ class ContainerThread(threading.Thread):
 
     def process_containers(self):
         for docker_host in self.docker_hosts:
+            ondemand_containers = docker_host["ondemand_containers"]
             docker_client = docker_host["docker_client"]
             containers = docker_client.containers.list(all=True, filters={ "label": ["swag_ondemand=enable"] })
             container_names = {container.name for container in containers}
 
-            for container_name in list(self.ondemand_containers.keys()):
+            for container_name in list(ondemand_containers.keys()):
                 if container_name in container_names:
                     continue
-                self.ondemand_containers.pop(container_name)
+                ondemand_containers.pop(container_name)
                 logging.info(f"Stopped monitoring {container_name}")
 
             for container in containers:
                 default_url = container.labels.get("swag_url", f"{container.name}.").rstrip("*")
                 container_urls = container.labels.get("swag_ondemand_urls", f"https://{default_url},http://{default_url}")
-                if container.name not in self.ondemand_containers.keys():
+                if container.name not in ondemand_containers.keys():
                     last_accessed = datetime.now()
                     logging.info(f"Started monitoring {container.name} for urls: {container_urls}")
                 else:
-                    last_accessed = self.ondemand_containers[container.name]["last_accessed"]
-                    if container_urls != self.ondemand_containers[container.name]["urls"]:
+                    last_accessed = ondemand_containers[container.name]["last_accessed"]
+                    if container_urls != ondemand_containers[container.name]["urls"]:
                         logging.info(f"Updated urls for {container.name} to: {container_urls}")
-                self.ondemand_containers[container.name] = { "docker_client": docker_client, "status": container.status, "urls": container_urls, "last_accessed": last_accessed }
+                ondemand_containers[container.name] = { "docker_client": docker_client, "status": container.status, "urls": container_urls, "last_accessed": last_accessed }
 
     def stop_containers(self):
-        for container_name in self.ondemand_containers.keys():
-            if self.ondemand_containers[container_name]["status"] != "running":
-                continue
-            inactive_seconds = (datetime.now() - self.ondemand_containers[container_name]["last_accessed"]).total_seconds()
-            if inactive_seconds < STOP_THRESHOLD:
-                continue
-            docker_client = self.ondemand_containers[container_name]["docker_client"]
-            docker_client.containers.get(container_name).stop()
-            logging.info(f"Stopped {container_name} after {STOP_THRESHOLD}s of inactivity")
+        for docker_host in self.docker_hosts:
+            ondemand_containers = docker_host["ondemand_containers"]
+            for container_name in ondemand_containers.keys():
+                if ondemand_containers[container_name]["status"] != "running":
+                    continue
+                inactive_seconds = (datetime.now() - ondemand_containers[container_name]["last_accessed"]).total_seconds()
+                if inactive_seconds < STOP_THRESHOLD:
+                    continue
+                docker_client = ondemand_containers[container_name]["docker_client"]
+                docker_client.containers.get(container_name).stop()
+                logging.info(f"Stopped {container_name} after {STOP_THRESHOLD}s of inactivity")
 
     def start_containers(self):
         with last_accessed_urls_lock:
             last_accessed_urls_combined = ",".join(last_accessed_urls)
             last_accessed_urls.clear()
 
-        for container_name in self.ondemand_containers.keys():
-            accessed = False
-            for ondemand_url in self.ondemand_containers[container_name]["urls"].split(","):
-                if ondemand_url not in last_accessed_urls_combined:
+        for docker_host in self.docker_hosts:
+            ondemand_containers = docker_host["ondemand_containers"]
+            for container_name in ondemand_containers.keys():
+                accessed = False
+                for ondemand_url in ondemand_containers[container_name]["urls"].split(","):
+                    if ondemand_url not in last_accessed_urls_combined:
+                        continue
+                    ondemand_containers[container_name]["last_accessed"] = datetime.now()
+                    accessed = True
+                if not accessed or ondemand_containers[container_name]["status"] == "running":
                     continue
-                self.ondemand_containers[container_name]["last_accessed"] = datetime.now()
-                accessed = True
-            if not accessed or self.ondemand_containers[container_name]["status"] == "running":
-                continue
-            docker_client = self.ondemand_containers[container_name]["docker_client"]
-            docker_client.containers.get(container_name).start()
-            logging.info(f"Started {container_name}")
-            self.ondemand_containers[container_name]["status"] = "running"
+                docker_client = ondemand_containers[container_name]["docker_client"]
+                docker_client.containers.get(container_name).start()
+                logging.info(f"Started {container_name}")
+                ondemand_containers[container_name]["status"] = "running"
 
     def run(self):
         while True:
